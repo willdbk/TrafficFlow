264e3a2e806c20e46c0c868f6fe85d68fa9b969a
Will deBruynKops <willdbk@gmail.com>
2019-12-08 15:00:05 -0500

    fixed

--- traffic_flow.py
+++ traffic_flow.py
@@ -1,60 +1,59 @@
 import math
 import numpy as np
-import scipy.linalg as sla
 import matplotlib.pyplot as plt
 from scipy.integrate import odeint
-import sys
 
-
-
+# The traffic flow class allows us to model traffic flow. It uses the method
+# of lines along with a High-resolution shock-capturing schemes (HRSC).
 class TrafficFlow:
+    # Initializes the
     def __init__(self, rho_bar, delta_rho, N = 10, x_center = 0.5, lam = 0.1, rho_max = 1, u_max = 1):
-        # self.rho_bar = rho_bar
-        # self.delta_rho = delta_rho
         self.u_max = u_max
         self.rho_max = rho_max
         self.delta_x = 1./N
         self.N = N
-        self.rho_vec = np.zeros(N)
+        self.rho_init_vec = np.zeros(N)
         self.x_vec = np.zeros(N)
         for i in range(N):
             self.x_vec[i] = (i+0.5)/N
-            self.rho_vec[i] = rho_bar + delta_rho*math.exp(-((self.x_vec[i] - x_center)/lam)**2)
+            self.rho_init_vec[i] = rho_bar + delta_rho*math.exp(-((self.x_vec[i] - x_center)/lam)**2)
 
     def compute_rho(self, t, M):
         self.M = M
         self.t_vec = np.linspace(0, t, M)
-        self.rho_mat = odeint(self.derivs, self.rho_vec, self.t_vec)
+        self.rho_mat = odeint(self.derivs, self.rho_init_vec, self.t_vec)
 
     def derivs_HRSC(self, rho_vec, t):
+        # one array to store parallel values for
         rho_vec_half_L = np.zeros(self.N)
         rho_vec_half_R = np.zeros(self.N)
         for i in range(self.N):
-            rho_vec_half_L[i] = self.rho_vec[self.left(i)] + \
-                0.5*self.minmod(self.rho_vec[i]-self.rho_vec[self.left(i)], \
-                self.rho_vec[self.left(i)]-self.rho_vec[self.left(self.left(i))])
-            rho_vec_half_R[i] = self.rho_vec[i] - \
-                0.5*self.minmod(self.rho_vec[self.right(i)]-self.rho_vec[i], \
-                self.rho_vec[i]-self.rho_vec[self.left(i)])
+            rho_vec_half_L[i] = rho_vec[i] + \
+                0.5*self.minmod(rho_vec[self.right(i)]-rho_vec[i], \
+                rho_vec[i]-rho_vec[self.left(i)])
+            rho_vec_half_R[i] = rho_vec[self.right(i)] - \
+                0.5*self.minmod(rho_vec[self.right(self.right(i))] - \
+                rho_vec[self.right(i)], rho_vec[self.right(i)]- \
+                rho_vec[i])
         f_vec_half = np.zeros(self.N)
         for i in range(self.N):
             s = 0
             if (rho_vec_half_L[i] - rho_vec_half_R[i]) != 0:
-                s = (self.f(rho_vec_half_L[i]) - self.f(rho_vec_half_R[i]))/(rho_vec_half_L[i] - rho_vec_half_R[i])
-                print(s)
+                s = (self.f(rho_vec_half_L[i]) - self.f(rho_vec_half_R[i]))/ \
+                    (rho_vec_half_L[i] - rho_vec_half_R[i])
             if s>0:
                 f_vec_half[i] = self.f(rho_vec_half_L[i])
             else:
                 f_vec_half[i] = self.f(rho_vec_half_R[i])
         rho_vec_dt = np.zeros(self.N)
         for i in range(self.N):
-            rho_vec_dt[i] = -1/(self.delta_x)*(f_vec_half[self.right(i)] - f_vec_half[i])
+            rho_vec_dt[i] = -1/(self.delta_x)*(f_vec_half[i] - f_vec_half[self.left(i)])
         return rho_vec_dt
 
     def derivs(self, rho_vec, t):
         rho_vec_dt = np.zeros(self.N)
         for i in range(self.N):
-            rho_vec_dt[i] = -1/(2*self.delta_x)*(self.f(rho_vec[self.left(i)]) - self.f(rho_vec[self.right(i)]))
+            rho_vec_dt[i] = -1/(2*self.delta_x)*(self.f(rho_vec[self.right(i)]) - self.f(rho_vec[self.left(i)]))
         return rho_vec_dt
 
 
@@ -79,9 +78,6 @@ class TrafficFlow:
                 return b
         return 0
 
-    def print_rho(self):
-        print(self.rho_vec)
-
     def graph_rho(self, num_plots):
         for i in range(len(self.t_vec)):
             if i%int(self.M/num_plots) == 0:
@@ -93,10 +89,10 @@ class TrafficFlow:
         plt.show()
 
 def main():
-    flow = TrafficFlow(0.5, 10e-3, 29)
-    t = 10
+    flow = TrafficFlow(0.5, 10e-3, 299)
+    t = 20
     flow.compute_rho(t, 5*t+1)
-    flow.graph_rho(5)
+    flow.graph_rho(10)
 
 if __name__ == "__main__":
     main()
